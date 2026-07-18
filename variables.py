10c3cccaad4262463b999745a8208bc715b04972
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 13:01:59 +0530

    types of variable assignments in python

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Variables in Python
+num = 5
+name = "Alex"  
+print(num)
+print(name)
+
+
+# Assigning Different Values to multiple variables
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
+
+# Assigning Same values to multiple variables
+a = b = c = 100
+print(a, b, c)
+
+# Dynamic allocation of variables in Python
+x = 10
+x = "Now a string"
+print("x:", x) # x: Now a string
