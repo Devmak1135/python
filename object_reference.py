c4f182de18b985b5c39e00f4a26d995edc8defe3
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 12:43:13 +0530

    concept of Object Referencing

---
+++ object_reference.py
@@ -0,0 +1,16 @@
+x=5
+y=x
+
+print("x:", x) # x: 5
+print("y:", y) # y: 5
+
+
+x = "Developer"
+
+print("x:", x) # x: Developer
+print("y:", y) # y: 5
+
+y = "Python"
+
+print("x:", x) # x: Developer
+print("y:", y) # y: Python
