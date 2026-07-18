9a411aad59a6252b89cdde8add84db99286cc34f
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 13:08:44 +0530

    Comparison Operators in Python

---
+++ comp.py
@@ -0,0 +1,11 @@
+# Comparison Operators in Python
+
+a = 13
+b = 33
+
+print(a > b) # False
+print(a < b) # True
+print(a == b) # False
+print(a != b) # True
+print(a >= b) # False
+print(a <= b) # True
