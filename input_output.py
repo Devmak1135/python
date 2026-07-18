29567f2fb1a020724c5844ac60570cca2932db19
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 12:29:07 +0530

    input and output

---
+++ input_output.py
@@ -0,0 +1,13 @@
+#Taking Input using input()
+name = input("Enter your name: ")
+print("Hello, " + name + "! Welcome to the program.")
+
+
+#Printing the Variables
+subject = "python"
+print(subject)
+
+person = "John"
+verb = "is learning"
+
+print(person + " " + verb + " " + subject + ".")
