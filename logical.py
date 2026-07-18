7a05a2c9f809a5e021d0ac14c788c05208a56142
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 13:15:17 +0530

    Logical Operators in python

---
+++ logical.py
@@ -0,0 +1,9 @@
+#Logical Operators
+#Logical operators perform Logical AND, Logical OR and Logical NOT operations. 
+#It is used to combine conditional statements.
+
+a = True
+b = False
+print(a and b)  # False
+print(a or b)   # True
+print(not a)    # False
