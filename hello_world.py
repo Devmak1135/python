7f4106af4de11c35898cbb9e11d7f4d91bc4b28d
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-18 12:33:19 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
 print("Hello, Developer!")
+
+#Printing the Variables
+subject = "python"
+print(subject)
+
+person = "John"
+verb = "is learning"
+
+print(person + " " + verb + " " + subject + ".")
