9588e5a9045515f77afcfeeb9c16e7cc0741427f
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2026-07-21 09:22:51 +0530

    Bitwise Operator

---
+++ bitwise.py
@@ -0,0 +1,14 @@
+# Bitwise Operators
+# Bitwise operators act on bits and perform bit-by-bit operations. 
+# These are used to operate on binary numbers.
+
+
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >> 2)
+print(a << 2)
